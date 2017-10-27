a7ffb382859a89637d411d643d58b0c80ccd9b59
lepor <lepor@LAPTOP-S331DEQO>
2017-10-27 19:54:19 +0200

    Nouvelle class

---
+++ pokedeck/src/main/java/upmc/pcg/Menu.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package upmc.pcg;
+import java.util.Scanner;
+
+/**
+ *
+ * @author lepor
+ */
+public class Menu {
+    public static void main(String[] args)
+  {
+
+  // Création joueur N°1
+  System.out.println("[1] Ajouter une nouvelle carte");
+  System.out.flush();
+  Scanner console = new Scanner(System.in);
+  int choice1 = console.nextInt();
+  
+  // Création joueur N°1
+  System.out.println("[2] Supprimer une carte");
+  System.out.flush();
+  int choice2 = console.nextInt();
+
+  }  
+}
