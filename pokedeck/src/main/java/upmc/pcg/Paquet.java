13db66a11670fc8feb8023e47307aa58986f630d
NICO <NICO@LAPTOP-LBTNGNDH>
2017-11-02 23:23:06 +0100

    arraylist deck - Nico

--- pokedeck/src/main/java/upmc/pcg/Paquet.java
+++ pokedeck/src/main/java/upmc/pcg/Paquet.java
@@ -8,10 +8,10 @@ import upmc.pcg.game.EnergyType;
 import upmc.pcg.game.PokemonCard;
 import static upmc.pcg.game.PokemonCard.getCarte;
 
-import static upmc.pcg.game.PokemonCard.jeu;
-import static upmc.pcg.game.PokemonCard.jeu1;
-import static upmc.pcg.game.PokemonCard.jeu2;
-import static upmc.pcg.game.PokemonCard.jeu3;
+import static upmc.pcg.game.PokemonCard.deck;
+//import static upmc.pcg.game.PokemonCard.jeu1;
+//import static upmc.pcg.game.PokemonCard.jeu2;
+//import static upmc.pcg.game.PokemonCard.jeu3;
 /**
  *
  * @author lepor
@@ -22,19 +22,20 @@ public class Paquet {
 
     public static void afficherCarte(){
         System.out.println(getNameCard()+" | "+getHpCard()+" | "+getEnergyCard());
+        
     
     }
 
     private static String getNameCard() {
-       return jeu1.get(0);
+       return deck.get(0);
     }
     
     private static int getHpCard() {
-       return jeu2.get(0);
+       return deck.get(0);
     }
     
     private static EnergyType getEnergyCard() {
-       return jeu3.get(0);
+       return deck.get(0);
     }
 
  
