a7cf1bd736e77fbc85eb9dedc882f751947b0403
NICO <NICO@LAPTOP-LBTNGNDH>
2017-12-04 22:05:11 +0100

    Menu - Nico

--- pokedeck/src/main/java/upmc/pcg/Menu.java
+++ pokedeck/src/main/java/upmc/pcg/Menu.java
@@ -1,70 +1,87 @@
-// Copyright 2017 Pierre Talbot (IRCAM)
+package Pokedeck.utilisateur;
 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-
-//     http://www.apache.org/licenses/LICENSE-2.0
-
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-package upmc.pcg;
-
-import java.util.Scanner; //Class scanner
-
-
+import java.util.Scanner;
 
 public class Menu {
-  
-   public void start()
-  {
-  
- 
-    Scanner sc = new Scanner(System.in);
-    System.out.println("[1] Add a card");
-    System.out.println("[2] Delete a card");
-    System.out.println("[3] Update a card");
-    System.out.println("[4] View my deck");
-    System.out.println("[5] Exit");
-    int choice = sc.nextInt();
-  
-    switch(choice) {
-        case 1: if (choice == 1) { 
-            addCard ajoutc = new addCard(); //run addCard class
-            ajoutc.askAddCard(); //run method askAddCard
-        }
-    
-        case 2: if (choice == 2) { 
-            delCard supprc = new delCard(); //run delCard class
-            supprc.askDelCard(); //run method askDelCard
-            break;
-        }
-    
-        case 3: if (choice == 3) {
-            System.out.println("Functionality not yet available");
-        }
-      
-        case 4: if (choice == 4) { 
-            Deck paquetdecarte = new Deck(); 
-            paquetdecarte.displayCard();
-            break;
-        }
-    
-        case 5 : if(choice == 5) {
-            System.out.println("You will leave the program...");
-            System.exit(0);
-        }
-    
-        case 6 : if(choice != 1 && choice  != 2 && choice != 3 && choice != 4 && choice != 5) {
-            System.out.println("Please enter an existing value :");
-            start();
-        }
-    }
+	
+	private Scanner sc;
+	private SousMenu sousMenu;
+	private String choix = "1";
+	private Deck paquetJoueur;
+		
+	public Menu(Deck d, Scanner scanner) {
+		this.sc = scanner;
+		sousMenu = new SousMenu(scanner);
+		paquetJoueur = d;
+	}
+	
+	public void depart() {
+		System.out.println("BONJOUR UTILISATEUR");
+		System.out.println("Que voulez-vous faire ?");
+		System.out.println("------------------------");
+		
+		while (Integer.parseInt(choix) != 0) {
+			menu();
+			choix_utilisateur();
+		}
+	}
+	
+	public void menu() {
+		System.out.println("--------- MENU ---------");
+		System.out.println("0 - QUITTER");
+		System.out.println("1 - VOIR POKEDECK");
+		System.out.println("2 - AJOUTER CARTE (POKEMON, ENERGIE, DRESSEUR)");
+		System.out.println("3 - SUPPRIMER CARTE");
+		System.out.println("4 - MODIFIER CARTE (AJOUTER DESCRIPTION)");
+		System.out.println("5 - RECHERCHER (DIFFERENTES OPTIONS)");
+	}
+	
+	public void choix_utilisateur() {
+		
+		choix = sc.nextLine();
+		if (choix.equals("")) {
+			choix = "0";
+			System.out.println("Choix vide --- QUITTER");
+		}
+		System.out.println("------------------------");
+		
+		int n = Integer.parseInt(choix);
+		
+		switch(n) {
+			case 0 :
+				// QUITTER
+				System.out.println("Vous venez de quitter.");
+				System.out.println("------------------------");
+				break;
+			case 1 :
+				// VOIR POKEDECK
+				sousMenu.parcoursDeck(paquetJoueur);
+				System.out.println("------------------------");
+				break;
+			case 2 :
+				// AJOUTER CARTE
+				sousMenu.ajoutCarteDansDeck(paquetJoueur);
+				System.out.println("------------------------");
+				break;
+			case 3 :
+				// SUPPRIMER CARTE
+				sousMenu.supprimerCarteDansDeck(paquetJoueur);
+				System.out.println("------------------------");
+				break;
+			case 4 :
+				// MODIFIER CARTE
+				sousMenu.modifierCarteDansDeck(paquetJoueur);
+				System.out.println("------------------------");
+				break;
+			case 5 :
+				// RECHERCHER CARTE
+				sousMenu.rechercherCarte(paquetJoueur);
+				System.out.println("------------------------");
+				break;
+			default :
+				System.out.println("Ce n'est pas un choix valide !");
+				break;
+		}
 
-  }   
-   
-   
+	}
 }
