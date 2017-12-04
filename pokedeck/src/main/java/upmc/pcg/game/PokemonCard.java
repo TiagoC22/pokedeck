21396aad439574f49cd26e43efa8955a81c4442e
Tiago <Tiago@LAPTOP-S331DEQO.home>
2017-12-04 23:43:17 +0100

    PokemonCard amélioration des getters + méthodes avec ajout super() pour constructeur classe Card. Classe fonctionnel

--- pokedeck/src/main/java/upmc/pcg/game/PokemonCard.java
+++ pokedeck/src/main/java/upmc/pcg/game/PokemonCard.java
@@ -19,10 +19,11 @@ public class PokemonCard extends Card {
     private String namePokemon;
     public static EnergyType energyType;
     private int hp;
+    protected int id;
   
     //Constructor
-    public PokemonCard(String namePokemon, int hp, EnergyType energyType) {
-        this.namePokemon = namePokemon;
+    public PokemonCard(int id, String namePokemon, int hp, EnergyType energyType) {
+        super(id, namePokemon, "");
         this.hp = hp;
         this.energyType = energyType;
     }
@@ -50,6 +51,11 @@ public class PokemonCard extends Card {
    
     public String msgPokemon() {
         return "You have create "+namePokemon+" with "+hp+" life points,"+" and with the energy "+energyType;
+    }
+
+    @Override
+    public String cardToString() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 
