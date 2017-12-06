6bc640b6608fb2a30e031d2a730fba45a37d7c85
Tiago <Tiago@LAPTOP-S331DEQO.home>
2017-12-06 23:47:12 +0100

    méthode jeu via console

--- pokedeck/src/main/java/upmc/pcg/ui/GameConsole.java
+++ pokedeck/src/main/java/upmc/pcg/ui/GameConsole.java
@@ -3,9 +3,13 @@ package upmc.pcg.ui;
 
 
 import java.util.Scanner;
-
+import upmc.pcg.Card;
 import upmc.pcg.Deck;
-
+import upmc.pcg.game.EnergyCard;
+import upmc.pcg.game.EnergyType;
+import upmc.pcg.game.PokemonCard;
+import upmc.pcg.game.TrainerCard;
+import upmc.pcg.game.TrainerType;
 
 
 public class GameConsole {
@@ -14,8 +18,8 @@ public class GameConsole {
 	private String choice;
 	private int value;
 
-	public GameConsole(Scanner sc) {
-		this.scanner = sc;
+	public GameConsole(Scanner scanner) {
+		this.scanner = scanner;
 	}
 
 	
@@ -28,19 +32,41 @@ public class GameConsole {
 		}
 	}
 
-	
 
 	public int choiceTypeOfCard() {
-		System.out.println("Vous voulez ajouter une carte... ?");
 		System.out.println("1 / Pokemon");
-                System.out.println("2 / Dresseur");
-                System.out.println("3 / Energie");
-                
+                System.out.println("2 / Trainer");
+                System.out.println("3 / Energy");
 		choice = scanner.nextLine();
 		int value = Integer.parseInt(choice);
 		return value;
 	}
 
-	
+	public Card displayCard(Deck game, int value) {
+		
+		switch (value) {
+                case 1: //Case pokemon
+                    int id;
+			
+			
+			
+                        // Choix de l'Energie
+			System.out.println("Energy of pokemon :");
+			int i = 0;
+			for (EnergyType type : EnergyType.values()) {
+				System.out.println(i + " - " + type);
+				i++;
+			}
 
+			
+			choice = scanner.nextLine();
+			value = Integer.parseInt(choice);
+			EnergyType energy = EnergyType.values()[value];
+
+			// Choix du nom du Pokemon
+			System.out.println("Name of pokemon :");
+			String namePokemon = scanner.nextLine();
+
+			
+			
 }
