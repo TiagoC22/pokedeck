9848c334fa50a97b378af6ff0e501aa18e2c893b
Tiago <Tiago@LAPTOP-S331DEQO.home>
2017-12-07 00:59:15 +0100

    Indentation classe "Card"

--- pokedeck/src/main/java/upmc/pcg/Card.java
+++ pokedeck/src/main/java/upmc/pcg/Card.java
@@ -1,36 +1,36 @@
 package upmc.pcg;
 
 public abstract class Card {
-        protected String typeCard;
-	protected String nameCard;
-	protected String description;
-        protected int id;
+    protected String typeCard;
+    protected String nameCard;
+    protected String description;
+    protected int id;
         
-        //Constructor
-        public Card(int id, String nameCard, String typeCard) {
+    //Constructor
+    public Card(int id, String nameCard, String typeCard) {
         this.id = id;
-	this.typeCard = typeCard;
-	this.nameCard = nameCard;
-	}
+        this.typeCard = typeCard;
+        this.nameCard = nameCard;
+    }
 
-	//Getters
-	public String getCardType() {
-		return typeCard;
-	}
+    //Getters
+    public String getCardType() {
+	return typeCard;
+    }
 	
-	public String getCardName() {
-		return nameCard;
-	}
+    public String getCardName() {
+        return nameCard;
+    }
         
-        public int getIdCard() {
-		return id;
-	}
+    public int getIdCard() {
+        return id;
+    }
 	
-        
-	public void cardDescription(String description) {
-		this.description = description;
-	}      
-       
-        public abstract String cardToString();
+    public void cardDescription(String description) {
+        this.description = description;
+    }      
+    
+    //toString for data of card
+    public abstract String cardToString();
 }
 
