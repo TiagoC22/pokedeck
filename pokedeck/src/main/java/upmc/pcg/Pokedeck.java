99b32ed177f0c3ba7596fb98ee9d61f3c0639ea3
Tiago <Tiago@LAPTOP-S331DEQO.home>
2017-12-07 18:55:49 +0100

    Indentation classe "Pokedeck"

--- pokedeck/src/main/java/upmc/pcg/Pokedeck.java
+++ pokedeck/src/main/java/upmc/pcg/Pokedeck.java
@@ -1,21 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
 package upmc.pcg;
-
-
-
 import java.util.Scanner;
 
 
 public class Pokedeck
 {
-  public static void main(String[] args)
-  {
-      Deck game = new Deck();
-      Scanner scanner =  new Scanner(System.in);
-      Menu menu = new Menu(game, scanner);
-      menu.start();
-  }
-  
-
+    public static void main(String[] args)
+    {
+        Deck game = new Deck();
+        Scanner scanner =  new Scanner(System.in);
+        Menu menu = new Menu(scanner, game);
+        menu.start(); //Run the pokedeck programm
+    }
 }
   
   
