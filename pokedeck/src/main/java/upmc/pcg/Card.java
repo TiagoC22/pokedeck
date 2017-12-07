01f554ef8dc46ef17efd5903786ca77988925c0b
Tiago <Tiago@LAPTOP-S331DEQO.home>
2017-12-07 01:04:41 +0100

    Indentation classe "Deck"

--- pokedeck/src/main/java/upmc/pcg/Card.java
+++ pokedeck/src/main/java/upmc/pcg/Card.java
@@ -1,4 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
 package upmc.pcg;
+
+/**
+ *
+ * @author Tiago
+ */
 
 public abstract class Card {
     protected String typeCard;
--- pokedeck/src/main/java/upmc/pcg/Deck.java
+++ pokedeck/src/main/java/upmc/pcg/Deck.java
@@ -1,79 +1,84 @@
-package upmc.pcg;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
+package upmc.pcg;
 import java.util.ArrayList;
 
-
+/**
+ *
+ * @author Tiago
+ */
 
 public class Deck {
-	
-	protected ArrayList<Card> game;
-	
-	public Deck() {
-		game = new ArrayList<Card>();
-	}
-	
-	public ArrayList<Card> getDeck() {
-		return game;
-	}
-	
-	public void addCard(Card c) {
-		game.add(c);
-	}
+    protected ArrayList<Card> game;
+
+    public Deck() {
+        game = new ArrayList<Card>();
+    }
+
+    public ArrayList<Card> getDeck() {
+        return game;
+        }
+
+    public void addCard(Card c) {
+        game.add(c);
+    }
         
-        public int sizeDeck() {
-		return game.size();
+    public int sizeDeck() {
+        return game.size();
+    }
+
+    public void addDescriptionCard(int id, String description) {
+        for (Card c : game) {
+            if (c.getIdCard() == id) c.cardDescription(description);
+        }
+    }
+
+    public boolean delCard(int id) {
+        for (Card c : game) {
+            if (c.getIdCard() == id) 
+            return game.remove(c);
+        }		
+        return false;
+    }
+
+    public void seeDeck() { 
+        for (Card c : game) {
+            System.out.println(c.cardToString());
 	}
-	
-        public void addDescriptionCard(int id, String description) {
-		for (Card c : game) {
-			if (c.getIdCard() == id) c.cardDescription(description);
-		}
+    }
+
+    public void displayCardType(String type) {
+	for (Card c : game) {
+            if (c.getCardType().equals(type)) 
+                System.out.println(c.cardToString());
 	}
-        
-	public boolean delCard(int id) {
-		for (Card c : game) {
-			if (c.getIdCard() == id) 
-                            return game.remove(c);
-		}		
-                    return false;
+    }
+
+    public void displayCardName(String name) {
+    for (Card c : game) {
+	String nameCard = c.getCardName();
+            if (nameCard.toLowerCase().equals(name.toLowerCase())) 
+                System.out.println(c.cardToString());
 	}
-	
-	public void seeDeck() { 
-		for (Card c : game) {
-			System.out.println(c.cardToString());
-		}
+    }
+
+    public boolean verifyDeck(int id) {
+	for (Card c : game) {
+            if (c.getIdCard() == id) 
+                return true;
 	}
-	
-        public void displayCardType(String type) {
-		for (Card c : game) {
-			if (c.getCardType().equals(type)) 
-                            System.out.println(c.cardToString());
-		}
+        return false;
+    }
+
+    public Card cardToDeck(int id) {
+	for (Card c : game) {
+            if (c.getIdCard() == id) 
+                return c;
 	}
-	
-	public void displayCardName(String name) {
-		String nameCard;
-		for (Card c : game) {
-			nameCard = c.getCardName();
-			if (nameCard.toLowerCase().equals(name.toLowerCase())) 
-                            System.out.println(c.cardToString());
-		}
-	}
-        
-	public boolean verifyDeck(int id) {
-		for (Card c : game) {
-			if (c.getIdCard() == id) 
-                            return true;
-		}
-                    return false;
-	}
-	
-	public Card cardToDeck(int id) {
-		for (Card c : game) {
-			if (c.getIdCard() == id) 
-                            return c;
-		}
-                    return null;
-	}
-	
+        return null;
+    }	
 }
